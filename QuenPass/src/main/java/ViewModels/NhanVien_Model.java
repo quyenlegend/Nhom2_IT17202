29be38c8481c47955da4fbfd2313e9f09f30e296
CuongDinh99 <cuongdhph13469@fpt.edu.vn>
2022-08-01 18:03:58 +0700

    quen mat khau

---
+++ QuenPass/src/main/java/ViewModels/NhanVien_Model.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ViewModels;
+
+import DomainModels.NhanVien;
+
+public class NhanVien_Model {
+
+    private String MaNV;
+    private String TenNV;
+    private boolean GioiTinh;
+    private String SDT;
+    private int ChucVu;
+    private String MatKhau;
+    private String Email;
+    private String DiaChi;
+    private boolean TrangThai;
+
+    public NhanVien_Model() {
+    }
+
+    public NhanVien_Model(String MaNV, String TenNV, boolean GioiTinh, String SDT, int ChucVu, String MatKhau, String Email, String DiaChi, boolean TrangThai) {
+        this.MaNV = MaNV;
+        this.TenNV = TenNV;
+        this.GioiTinh = GioiTinh;
+        this.SDT = SDT;
+        this.ChucVu = ChucVu;
+        this.MatKhau = MatKhau;
+        this.Email = Email;
+        this.DiaChi = DiaChi;
+        this.TrangThai = TrangThai;
+    }
+
+    public String getMaNV() {
+        return MaNV;
+    }
+
+    public void setMaNV(String MaNV) {
+        this.MaNV = MaNV;
+    }
+
+    public String getTenNV() {
+        return TenNV;
+    }
+
+    public void setTenNV(String TenNV) {
+        this.TenNV = TenNV;
+    }
+
+    public boolean isGioiTinh() {
+        return GioiTinh;
+    }
+
+    public void setGioiTinh(boolean GioiTinh) {
+        this.GioiTinh = GioiTinh;
+    }
+
+    public String getSDT() {
+        return SDT;
+    }
+
+    public void setSDT(String SDT) {
+        this.SDT = SDT;
+    }
+
+    public int getChucVu() {
+        return ChucVu;
+    }
+
+    public void setChucVu(int ChucVu) {
+        this.ChucVu = ChucVu;
+    }
+
+    public String getMatKhau() {
+        return MatKhau;
+    }
+
+    public void setMatKhau(String MatKhau) {
+        this.MatKhau = MatKhau;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getDiaChi() {
+        return DiaChi;
+    }
+
+    public void setDiaChi(String DiaChi) {
+        this.DiaChi = DiaChi;
+    }
+
+    public boolean isTrangThai() {
+        return TrangThai;
+    }
+
+    public void setTrangThai(boolean TrangThai) {
+        this.TrangThai = TrangThai;
+    }
+
+}
